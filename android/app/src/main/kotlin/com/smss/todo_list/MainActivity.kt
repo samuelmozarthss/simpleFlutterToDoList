7036c5663ceaa26b6049319e9a2787b976622852
samuelmozarthss <contatosamuelm@gmail.com>
2022-06-14 01:27:32 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/smss/todo_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smss.todo_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page/todo_list_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  TodoListPage({Key? key}) : super(key: key);
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'E-mail',
+                ),
+                onChanged: onChanged,
+                onSubmitted: onSubmitted,
+              ),
+              ElevatedButton(
+                onPressed: login,
+                child: Text('Entrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void login() {
+    String text = emailController.text;
+    print(text);
+    emailController.text = 'Samuel';
+  }
+
+  void onChanged(String text) {
+    //print(text);
+  }
+
+  void onSubmitted(String text) {
+    print(text);
+  }
+}
