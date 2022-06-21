27ae18d5dbbdb0d79b06667b945844150719d3f0
samuelmozarthss <contatosamuelm@gmail.com>
2022-06-20 23:41:20 -0300

    update screen

--- lib/page/todo_list_page.dart
+++ lib/page/todo_list_page.dart
@@ -8,19 +8,69 @@ class TodoListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Row(
-        children: [
-          const SizedBox(
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Adicione uma tarefa',
-                hintText: 'Ex: Estudar flutter',
-              ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                const Expanded(
+                  flex: 5,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Adicione uma tarefa',
+                      hintText: 'Ex: Estudar flutter',
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff023020),
+                      padding: const EdgeInsets.all(
+                        14,
+                      ),
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                )
+              ],
             ),
-          ),
-          ElevatedButton(onPressed: () {}, child: Text('+'))
-        ],
+            const SizedBox(
+              height: 8,
+            ),
+            Row(
+              children: [
+                const Expanded(
+                  child: Text('Você possui 0 tarefas pendentes'),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0xff023020),
+                    padding: const EdgeInsets.all(
+                      14,
+                    ),
+                  ),
+                  child: const Text('Limpar tudo'),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     ));
   }
